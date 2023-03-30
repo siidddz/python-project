64bee8e045299c9ffcc2d158d71599e51be6aec4
Siddharthsinh Parmar <108690974+siidddz@users.noreply.github.com>
2023-03-30 13:21:13 +0530

    Add files via upload

---
+++ Tutorial Programs/WhileLoop.py
@@ -0,0 +1,11 @@
+# Name :- Parmar Siddharth
+#---------------------------------------------------------
+#while-loop tutorial 
+
+n=int(input("Enter N:"))
+sum=0
+
+while n>0:
+    sum=sum+n
+    n=n-1
+print("Sum : ",sum)
